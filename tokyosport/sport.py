7427d28369341aa195727ec602ab813899cc5a78
shimpeko <shimpeko@gmail.com>
2015-10-02 19:09:03 +0900

    Fix typo

--- tokyosport/sport.py
+++ tokyosport/sport.py
@@ -6,8 +6,8 @@ class Sport(Enum):
     baseball_mini = 1012
     tennis_hard = 2021
     tennis_omni = 2023
-    soccor = 3031
-    soccor_mini = 3032
+    soccer = 3031
+    soccer_mini = 3032
 
 if __name__ == '__main__':
     for s in Sport:
