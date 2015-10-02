160f918c03ecad2134e8f444e8ca9b44f187b969
shimpeko <shimpeko@gmail.com>
2015-10-02 19:08:10 +0900

    Implement json output of parks

---
+++ util/make_park_lists.py
@@ -0,0 +1,36 @@
+import requests
+import unicodedata
+import re
+import json
+from tablestripper import TableStripper
+
+r = requests.get('https://yoyaku.sports.metro.tokyo.jp/web/html/kouenichiran110201.htm')
+
+ts = TableStripper()
+ts.feed(r.content.decode('shift_jis'))
+
+parks = {}
+ro = re.compile('.*-->(.*)$')
+
+for row in ts.get_rows():
+    # for 26大島小松川B
+    if len(row) == 18:
+        row.insert(4, "")
+    # exclude non park data
+    if len(row) > 18 and row[0] != '':
+        park = {}
+        park['id'] = int(unicodedata.normalize('NFKC', row[0]))
+        match = ro.match(row[2].replace('\r\n', ''))
+        if match:
+            park['name'] = match.group(1)
+        c = list(map(lambda s: unicodedata.normalize('NFKC', s), row[5:]))
+        counts = [int(cnt) if cnt != '' else 0 for cnt in c]
+        park['baseball'] = counts[0]
+        park['baseball_mini'] = counts[3]
+        park['tennis_hard'] = counts[6]
+        park['tennis_omni'] = counts[9]
+        park['soccer'] = counts[12]
+        park['soccer_mini'] = counts[13]
+        parks[park['id']] = park
+
+print(json.dumps(parks, ensure_ascii=False, indent=2))
